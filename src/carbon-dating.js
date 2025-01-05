9e440e5dbdeafc4c385e38caa3e3d032b0595b1f
MashaPu <mariya_pudovkina@mail.ru>
2025-01-05 21:38:14 +0500

    feat: task basic-js carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity === 'string')
+    if (/^\d+(\.\d+)?$/.sampleActivity) {
+    return Math.round ((Math.log (MODERN_ACTIVITY / (sampleActivity * 1))) / (0.693 / HALF_LIFE_PERIOD));
+  } else {
+    return false;
+  }
+
   // remove line with error and write your code here
 }
 
