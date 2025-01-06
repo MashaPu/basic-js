fa98c549042367b7e7a112bc7e17ca4193ece508
MashaPu <mariya_pudovkina@mail.ru>
2025-01-06 21:41:57 +0500

    feat: task basic-js sum-digits and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,8 +13,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (typeof members === 'string') {
+    return members.map (str => str.charAt (0)).sort().join('').toUpperCase(); 
+  } else {
+    return false;
+  }
   // remove line with error and write your code here
 }
 
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,8 +12,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function getSumOfDigits(n) {
+  let sum = n;
+  while (sum >= 10) {
+    sum = String(sum).split('').map(Number).reduce ((acc, curr) => acc + curr, 0);
+  } return sum;
   // remove line with error and write your code here
 }
 
